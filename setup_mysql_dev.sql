45bdba523c57ad39cb30b89a5e1cd2944239ea82
thomaspwang <thomaspatrickwang@gmail.com>
2017-11-14 21:36:50 +0000

    add .

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
