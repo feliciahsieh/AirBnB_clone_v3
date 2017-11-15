9d4431d96e175f12e12d7708ce976646d61130e3
thomaspwang <thomaspatrickwang@gmail.com>
2017-11-15 22:36:23 +0000

    fix place

--- models/place.py
+++ models/place.py
@@ -27,14 +27,13 @@ class Place(BaseModel, Base):
         city_id = Column(String(60),
                          ForeignKey("cities.id"),
                          nullable=False)
-
         user_id = Column(String(60),
                          ForeignKey('users.id'),
                          nullable=False)
         name = Column(String(128),
                       nullable=False)
         description = Column(String(1024),
-                             nullable=False)
+                             nullable=True)
         number_rooms = Column(Integer,
                               default=0,
                               nullable=False)
@@ -47,10 +46,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer,
                                 default=0,
                                 nullable=False)
-        latitude = Float(Float,
-                         nullable=False)
-        longitude = Float(Float,
-                         nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
         reviews = relationship("Review",
                                backref="places")
         amenities = relationship("Amenity",
