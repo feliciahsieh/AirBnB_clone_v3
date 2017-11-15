89ffaf9f36119d1f6998d76ca43d3bd58fc410e3
thomaspwang <thomaspatrickwang@gmail.com>
2017-11-15 01:12:00 +0000

    update state

--- models/state.py
+++ models/state.py
@@ -3,10 +3,27 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """Representation of state """
-    name = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128),
+                      nullable=False)
+        city = relationship("City",
+                            backref="state")
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def cities(self):
+        """fs getter attribute that returns City instances"""
+        values_city = models.storage.all("City").values
+        list_city = []
+        for city in values_city:
+            if city.state_id == self.id:
+                list_city.append(city)
+        return list_city
