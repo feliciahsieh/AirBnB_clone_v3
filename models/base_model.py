331b86f539f957935b27728279ad73c4768f1ba3
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-11-15 22:41:06 +0000

    Remove extra whitespace

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
         id = Column(String(60), nullable=False, primary_key=True)
         created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
         updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-        
+
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
         self.id = str(uuid.uuid4())
