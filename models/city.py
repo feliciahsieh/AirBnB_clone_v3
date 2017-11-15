69e14336aa09df8eb5fc908ae06bbb1c27ef97f6
thomaspwang <thomaspatrickwang@gmail.com>
2017-11-15 03:51:34 +0000

    update place

--- models/city.py
+++ models/city.py
@@ -17,6 +17,8 @@ class City(BaseModel, Base):
         state_id = Column(String(60),
                           ForeignKey('states.id'),
                           nullable=False)
+        places = relationship("Place",
+                              backref="cities")
     else:
         name = ""
         state_id = ""
