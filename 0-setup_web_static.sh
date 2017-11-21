8cf2a7ef364706248437844fd44c00cc66330b0b
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-11-21 04:14:02 +0000

    Correct configuration file for new installation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
 echo 'Testing 123' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
-insert='\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
-sed -i "37i $insert" /etc/nginx/nginx.conf
+insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
+sed -i "37i $insert" /etc/nginx/sites-available/default
 service nginx restart
 exit 0
