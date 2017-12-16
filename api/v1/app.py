a0650ec01f310cc96e3d11d05b6d9c2657c8bcaf
feliciahsieh <felicia@tixwriteoff.com>
2017-12-16 20:09:45 +0000

    For #6. Untested

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,7 +14,12 @@ app_views = Blueprint("app", __name__)
 #declare a method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
 def teardown_appcontext:
+    """teardown_appcontext"""
     storage.close()
+
+def htmlNotFoundError():
+    """htmlNotFoundError to handle HTML 404 Not Found errors"""
+    return '{\n\t"error": "Not found"\n}'
 
 if __name__ == "__main__":
     #run your Flask server (variable app) with:
