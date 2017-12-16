f8ff58c7e7cb87f04c7715af195e0918ca20f503
feliciahsieh <felicia@tixwriteoff.com>
2017-12-16 05:49:57 +0000

    fix bug with baseline

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
         __tablename__ = 'states'
         name = Column(String(128),
                       nullable=False)
-        cities = relationship("City",
+        cities = relationship("City", cascade="all, delete", #add cascade
                               backref="states")
     else:
         name = ""
