8e0eac73055644149d04013822d9ab1aebf3ec87
feliciahsieh <felicia@tixwriteoff.com>
2017-12-16 20:05:26 +0000

    first commit. Untested

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
 import app_views from api.v1.views
+hbnbClass = {
+    'Amenity': Amenity,
+    'City': City,
+    'Place': Place,
+    'State': State,
+    'Review': Review,
+    'User': User
+}
+
+hbnbText = {
+    "amenities",
+    "cities",
+    "places",
+    "reviews",
+    "states",
+    "users"
+}
 
 
 @app_views.route('/status')
@@ -11,4 +28,12 @@ def hbnbStatus():
 @app_views.route('/api/v1/stats')
 def hbnbStats():
     """hbnbStats"""
-    storage.count()
+    numClass = len(hbnbText)
+    str = '{\n\t"'
+    for count in range(num):
+        str += hbnbClass[count][0]
+        str += "\": "
+        str += str(storage.count(hbnbClass[count][0]))
+        if count < numClass:
+            str += ",\n"
+    str += "\n}\n"
