d75125a0901eb5b4757319e37448ed2f143984e9
feliciahsieh <felicia@tixwriteoff.com>
2017-12-20 23:52:34 +0000

    first commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Initialize Blueprint views"""
-from flask import Flask
 from flask import Blueprint
-import * from api.v1.views.index
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-#create a variable app_views which is an instance of Blueprint (url prefix must be /api/v1)
-app_views = Blueprint("app", __name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
-import app_views from api.v1.views
-hbnbClass = {
-    'Amenity': Amenity,
-    'City': City,
-    'Place': Place,
-    'State': State,
-    'Review': Review,
-    'User': User
-}
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+
 
 hbnbText = {
-    "amenities",
-    "cities",
-    "places",
-    "reviews",
-    "states",
-    "users"
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
 }
 
-
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     """hbnbStatus"""
-    return ('{\n\t"status": "OK"\n}')
+    return jsonify({"status": "OK"})
 
-@app_views.route('/api/v1/stats')
+@app_views.route('/api/v1/stats', strict_slashes=False)
 def hbnbStats():
     """hbnbStats"""
     numClass = len(hbnbText)
-    str = '{\n\t"'
-    for count in range(num):
-        str += hbnbClass[count][0]
-        str += "\": "
-        str += str(storage.count(hbnbClass[count][0]))
-        if count < numClass:
-            str += ",\n"
-    str += "\n}\n"
+    for key, value in hbnbText:
+        jsonify("{}{}".format(key, storage.count(value)))
+
+if __name__ == "__main__":
+    pass
