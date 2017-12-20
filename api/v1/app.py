e8491613bd0acdd4e4614917bf0d18ef3e9ca37e
feliciahsieh <felicia@tixwriteoff.com>
2017-12-20 23:48:22 +0000

    first commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-import storage from models
-import app_views from api.v1.views
-from flask import Flask
-from flask import Blueprint
+import os
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
 
 
-app = Flask('app')
-
-#register the blueprint app_views to your Flask instance app
-app_views = Blueprint("app", __name__)
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 #declare a method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
-def teardown_appcontext:
+def teardown_appcontext(code):
     """teardown_appcontext"""
     storage.close()
 
-def htmlNotFoundError():
-    """htmlNotFoundError to handle HTML 404 Not Found errors"""
-    return '{\n\t"error": "Not found"\n}'
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
 
 if __name__ == "__main__":
-    #run your Flask server (variable app) with:
-    #host = environment variable HBNB_API_HOST or 0.0.0.0 as default value
-    #port = environment variable HBNB_API_PORT or 5000 as default value
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=os.getenv('HBNB_API_HOST'), port=int(os.getenv('HBNB_API_PORT')))
